c22b88d7dadad184176ce8edc4d50c4da20c3b18
leesuin <suin0212@gmail.com>
2023-10-23 03:23:26 +0900

    Feature: 상세페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Route, Routes } from "react-router";
 
 import GlobalStyles from "styles/globalStyle";
-import MainPage from "pages/main";
+import DetailPage from "pages/detail";
 import { Header } from "components";
+import MainPage from "pages/main";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/:id" element={<DetailPage />} />
       </Routes>
     </div>
   );
