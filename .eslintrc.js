5b8475b5b80b1590360e0e46d2af346582bdfdc1
leesuin <suin0212@gmail.com>
2023-10-22 11:40:47 +0900

    Chore: eslint rule 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  // eslint에 어떠한 parser를 사용할지 알려주는 옵션
-  // eslint가 typescript 문법을 이해할 수 있게 해준다.
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -15,10 +13,7 @@ module.exports = {
     ecmaVersion: "latest",
     courceType: "module",
   },
-  // typescript-eslint에서 제공하는 규칙들을 사용할 수 있게 해준다.
   plugins: ["react", "@typescript-eslint", "prettier"],
-  // 어떠한 규칙들과 설정으로 eslint를 사용할지 명시한다.
-  // 아래와 같이 작성하면 default 값으로 적용이 된다.
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -28,14 +23,13 @@ module.exports = {
   ],
   rules: {
     "prettier/prettier": "error",
-    // 세미콜론이 없으면 에러로 취급한다.
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
     "no-var": "error",
     indent: ["error", 2],
     quotes: ["error", "double"],
     semi: ["error", "always"],
-    // 기존 프로젝트에서는 "warn"으로 취급되지만, "error"로 설정하면 에러로 취급한다.
     "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "off",
   },
   settings: {
     "import/parsers": {
